960e11d7a596dc99041c98bc47027a6b33bb4515
lauramesquitabruel <lauramesquitabruel@gmail.com>
2025-02-17 10:35:05 -0300

    controle de acesso por perfil e rota

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$12$uIQMCn2qN2/IPb4Z6zZ3Yu37/xZxvhPkYSIw5IOuXBEFe8stLQt.K');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$12$uIQMCn2qN2/IPb4Z6zZ3Yu37/xZxvhPkYSIw5IOuXBEFe8stLQt.K');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
