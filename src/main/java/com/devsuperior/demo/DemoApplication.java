ca42fb21e136f59518d81f2607d82cb05120f767
lauramesquitabruel <lauramesquitabruel@gmail.com>
2025-02-16 21:40:35 -0300

    erro ao gerar token

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -7,17 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+public class DemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
 }
